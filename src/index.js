67a0b9ae8372fd10dfd7b38b5638921f77c67a68
aaa <2238034790@qq.com>
2020-09-02 10:23:07 +0800

    配置babel后的第一次提交

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-01 09:45:00
+ * @LastEditTime: 2020-09-02 10:17:12
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \新建文件夹\index.js
+ */
+
+let b = ()=>{
+  return 'b'
+}
